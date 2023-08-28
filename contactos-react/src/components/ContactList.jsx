05c07572d0fb793b87971fce5ccb6a419e577a89
bocchiko <luisbonilla0908@gmail.com>
2023-08-28 15:49:36 -0600

    Contact & ContactList components

---
+++ contactos-react/src/components/ContactList.jsx
@@ -0,0 +1,32 @@
+export const ContactList = ({contacts, onRemove, setFavorite}) => {
+    
+    
+    const SortedList = [...contacts].sort((a, b) =>{
+        if (a.isFavorite && !b.isFavorite) return -1;
+        if (!a.isFavorite && b.isFavorite) return 1;
+        return 0;
+    })
+    return (
+       <>
+        {
+            SortedList.map((contact, index) => {
+                return (
+                    <Contact
+                        key={index}
+                        
+                        contact={contact}
+
+                        onRemove={onRemove}
+                        
+                        setFavorite={setFavorite}
+                    />
+                )
+            })
+        }
+       </>
+        
+
+    );
+
+
+}
--- contactos-react/src/components/contact.jsx
+++ contactos-react/src/components/contact.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Contact = ({ contact, RemoveContact, isFavorite }) => {
+const Contact = ({contact,onRemove, setFavorite}) => {
+
+
+
+    const[isFavorite,setFav] = useState(contact.isFavorite);
+
+    const ToggleFavorite = (e) => {
+        setFav(!isFavorite);
+        setFavorite(contact.id, contact.isFavorite  === true ? false:true );
+    };
+
     return (
-        <div className={`contact ${contact.isFavorite ? 'favorite' : ''}`}>
+        <div className={`contact ${isFavorite ? 'favorite' : ''}`}>
+            <p>{contact.id}</p>
             <p>{contact.name}</p>
+            <p>{contact.lastname}</p>
             <p>{contact.phoneNumber}</p>
-            <button onClick={() => isFavorite(contact.id)}>
+            <button onClick={ToggleFavorite}>
                 {contact.isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
             </button>
-            <button onClick={() => RemoveContact(contact.id)}>Remove</button>
+            <button onClick={() => onRemove(contact.id)}>Remove</button>
         </div>
-    );
+    ); 
 };
 
 export default Contact;
